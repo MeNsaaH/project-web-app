4b5460500c5712e9412312a8be394ba8c1c4d1aa
Manasseh Mmadu <mmadumanasseh@gmail.com>
2019-11-07 09:41:32 +0100

    Cleaned out installation

--- main/views.py
+++ main/views.py
@@ -15,21 +15,22 @@ class Index(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         current_obj = Record.objects.last()
-        last_hour = current_obj.date_created.hour
-        norm_last_hour = last_hour if last_hour <= 12 else last_hour - 12
-        # Add in a QuerySet of all the books
-        measured_water_level = list(reversed(
-            Record.objects.values_list('water_level', flat=True).order_by('-id')[:last_hour]))
-        predictions = [get_state_str(x) for x in reversed(
-            Prediction.objects.values_list('prediction', flat=True).order_by('-id')[:12])]
-        context['measured_water_level'] = json.dumps(measured_water_level)
-        context['measured_flood_state'] = json.dumps([get_state(x) for x in measured_water_level])
-        context['predictions'] = json.dumps(predictions)
-        context['measured_rainfall'] = Record.objects.values_list('rainfall_intensity', flat=True)
-        context['time_type'] = 'am' if last_hour <= 12 else 'pm'
-        context['next_state'] = predictions[norm_last_hour - 1]
-        context['notifications'] = Notification.objects.filter(read=False)[:5]
-        context['current_water_level'] = int((current_obj.water_level/settings.DRAIN_HEIGHT) * 100)
+        if current_obj:
+            last_hour = current_obj.date_created.hour
+            norm_last_hour = last_hour if last_hour <= 12 else last_hour - 12
+            # Add in a QuerySet of all the books
+            measured_water_level = list(reversed(
+                Record.objects.values_list('water_level', flat=True).order_by('-id')[:last_hour]))
+            predictions = [get_state_str(x) for x in reversed(
+                Prediction.objects.values_list('prediction', flat=True).order_by('-id')[:12])]
+            context['measured_water_level'] = json.dumps(measured_water_level)
+            context['measured_flood_state'] = json.dumps([get_state(x) for x in measured_water_level])
+            context['predictions'] = json.dumps(predictions)
+            context['measured_rainfall'] = Record.objects.values_list('rainfall_intensity', flat=True)
+            context['time_type'] = 'am' if last_hour <= 12 else 'pm'
+            context['next_state'] = predictions[norm_last_hour - 1]
+            context['notifications'] = Notification.objects.filter(read=False)[:5]
+            context['current_water_level'] = int((current_obj.water_level/settings.DRAIN_HEIGHT) * 100)
         return context
 
 
